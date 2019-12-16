d2a5697bcf768580bcb6eb6a688e67c23e168559
Sean Reece <reece.sean@gmail.com>
2019-12-16 15:05:50 -0500

    no pack

--- dist/wait.js
+++ dist/wait.js
@@ -8,28 +8,15 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
-    result["default"] = mod;
-    return result;
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const github = __importStar(require("@actions/github"));
-const core = __importStar(require("@actions/core"));
-function run() {
+function wait(milliseconds) {
     return __awaiter(this, void 0, void 0, function* () {
-        try {
-            const token = core.getInput('repo-token');
-            const octokit = new github.GitHub(token);
-            // console.log(github.context.repo)
-            const { data: pullRequest } = yield octokit.pulls.list(Object.assign({}, github.context.repo));
-            console.log(pullRequest);
-        }
-        catch (error) {
-            core.setFailed(error.message);
-        }
+        return new Promise(resolve => {
+            if (isNaN(milliseconds)) {
+                throw new Error('milliseconds not a number');
+            }
+            setTimeout(() => resolve('done!'), milliseconds);
+        });
     });
 }
-run();
+exports.wait = wait;
