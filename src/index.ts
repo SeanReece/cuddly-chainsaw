255d72fdf02c2eaae923891da3709972edb9857a
Sean Reece <reece.sean@gmail.com>
2019-12-16 15:42:45 -0500

    Do the slack

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import * as github from '@actions/github'
 import * as core from '@actions/core'
+import wretch from 'wretch'
 
 async function run(): Promise<void> {
   try {
     const token: string = core.getInput('repo-token')
+    const slackWebhook: string = core.getInput('slack_webhook')
 
     const octokit = new github.GitHub(token)
 
-    // console.log(github.context.repo)
+    const {data: pullRequests} = await octokit.pulls.list({...github.context.repo})
 
-    const {data: pullRequest} = await octokit.pulls.list({...github.context.repo})
+    console.log(pullRequests)
 
-    console.log(pullRequest)
+    const message = {
+      text: `Hello,\nYou have ${pullRequests.length} PRs!`,
+      username: 'Cuddly Chainsaw PR Notifications',
+      icon_emoji: ':ghost:'
+    }
+
+    const slackHook = wretch(slackWebhook)
+    const response = await slackHook.post(message).json()
+    console.log(response)
   } catch (error) {
     core.setFailed(error.message)
   }
