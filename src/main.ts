f6622cc8a6377aa41bff464fa67fe25bfe951df1
Sean Reece <reece.sean@gmail.com>
2019-12-16 14:48:30 -0500

    build it

--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,18 @@ import * as core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
-    const token: string = core.getInput('repo-token');
+    const token: string = core.getInput('repo-token')
 
-    const octokit = new github.GitHub(token);
+    const octokit = new github.GitHub(token)
 
     console.log(github.context)
 
-    const { data: pullRequest } = await octokit.pulls.list({
-        owner: 'octokit',
-        repo: 'rest.js'
-    });
+    const {data: pullRequest} = await octokit.pulls.list({
+      owner: 'octokit',
+      repo: 'rest.js'
+    })
 
-    console.log(pullRequest);
+    console.log(pullRequest)
   } catch (error) {
     core.setFailed(error.message)
   }
